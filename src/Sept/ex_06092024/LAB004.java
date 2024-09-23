5dc04921322f9c44923938dfd822cd7e74047b9b
Harleen Kaur <harleen@ehtgroup.com>
2024-09-22 20:26:36 -0400

    Java First Program

---
+++ src/Sept/ex_06092024/LAB004.java
@@ -0,0 +1,15 @@
+package Sept.ex_06092024;
+/* First Java Program
+   Author: Harleen Kaur
+*/
+public class LAB004 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("good");
+        System.out.println(5+8);
+        main1();
+    }
+    public static void main1() {
+        System.out.println("other function");
+    }
+}
