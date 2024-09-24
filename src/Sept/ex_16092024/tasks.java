316cf7d6926ffa96c5a422e7becc267369c1111d
Harleen Kaur <harleen@ehtgroup.com>
2024-09-23 22:55:20 -0400

    tasks 16sept

---
+++ src/Sept/ex_16092024/tasks.java
@@ -0,0 +1,12 @@
+package Sept.ex_16092024;
+// Task 5 output for given expression
+public class tasks {
+    public static void main(String[] args) {
+        short s = 10;
+        char c = 'A'; //65
+        int ss = s+c;
+        System.out.println(ss);
+        // As character is numeric data type 'A'=65
+        // output is integer value = 65+10 = 75
+    }
+}
