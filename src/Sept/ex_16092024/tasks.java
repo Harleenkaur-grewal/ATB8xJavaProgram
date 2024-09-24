bbb69531fffb6af7b66fdf76f13b135204d5b21a
Harleen Kaur <harleen@ehtgroup.com>
2024-09-23 23:10:59 -0400

    tasks 16sept

--- src/Sept/ex_16092024/tasks.java
+++ src/Sept/ex_16092024/tasks.java
@@ -1,7 +1,11 @@
 package Sept.ex_16092024;
-// Task 5 output for given expression
+// Task 4 & 5 output for given expression
 public class tasks {
     public static void main(String[] args) {
+        byte a = 10;
+        byte b = 10;
+        System.out.println(a+b);
+        //output is integer data type
         short s = 10;
         char c = 'A'; //65
         int ss = s+c;
