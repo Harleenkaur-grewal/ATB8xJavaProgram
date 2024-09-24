24f7412e55d3ea968466378ffe4dd603a4f56c05
Harleen Kaur <harleen@ehtgroup.com>
2024-09-23 22:11:14 -0400

    tasks 13sept

---
+++ src/Sept/ex_13092024/task1.java
@@ -0,0 +1,22 @@
+package Sept.ex_13092024;
+/*
+Write a program to print the table of number which user will give( int num 10)
+output will be by using the printf()
+ */
+public class task1 {
+    public static void main(String[] args) {
+        int num = 3;
+        int multiplier = 0;
+        System.out.printf("%d x %d = %d \n", num,multiplier,   num * (multiplier));
+        System.out.printf("%d x %d = %d \n", num,multiplier+ 1, num * (multiplier+1));
+        System.out.printf("%d x %d = %d \n", num,multiplier+ 2, num * (multiplier+ 2));
+        System.out.printf("%d x %d = %d \n", num,multiplier+ 3, num * (multiplier+ 3));
+        System.out.printf("%d x %d = %d \n", num,multiplier+ 4, num * (multiplier+ 4));
+        System.out.printf("%d x %d = %d \n", num,multiplier+ 5, num * (multiplier+ 5));
+        System.out.printf("%d x %d = %d \n", num,multiplier+ 6, num * (multiplier+ 6));
+        System.out.printf("%d x %d = %d \n", num,multiplier+ 7, num * (multiplier+ 7));
+        System.out.printf("%d x %d = %d \n", num,multiplier+ 8, num * (multiplier+ 8));
+        System.out.printf("%d x %d = %d \n", num,multiplier+ 9, num * (multiplier+ 9));
+        System.out.printf("%d x %d = %d \n", num,multiplier+ 10, num * (multiplier+ 10));
+    }
+}
---
+++ src/Sept/ex_13092024/task2.java
@@ -0,0 +1,19 @@
+package Sept.ex_13092024;
+/*
+Prepare a small calculator, which supports +, - , , / and %(?) - Modulus operator
+double a = 34;
+double b = 10;
+Print - a+b, a-b, ab, a/b, a%b all the outputs.
+ */
+public class task2 {
+    public static void main(String[] args) {
+        double a =93;
+        double b =67;
+        System.out.printf("Addition of two numbers %.2f and %.2f is %.2f  \n", a,b, a+b);
+        System.out.printf("Subtraction of two numbers %.2f and %.2f is %.2f  \n", a,b, a-b);
+        System.out.printf("Multiplication of two numbers %.2f and %.2f is %.2f  \n", a,b, a*b);
+        System.out.printf("Division of two numbers %.2f and %.2f is %.2f  \n", a,b, a/b);
+        System.out.printf("Modulus of two numbers %.2f and %.2f is %.2f  \n", a,b, a%b);
+    }
+}
+
